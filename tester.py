054c858312d55cea3555b1e45271a0d71f2ae192
Annalise Fishell PC <annalise.fishell@gmail.com>
2024-05-29 23:34:07 +0200

    added beginin of training

--- tester.py
+++ tester.py
@@ -5,11 +5,18 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cmasher as cmr
 import math
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.model import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dropout, LeakyReLU, MaxPooling2D, Dense
 
 
 LR = 0.001
 EPOCH = 100
 BS = 32
+NUM_FILTERS = 32
+NUM_LAYERS = 3
+ACTIVATION = 'relu'
+KERNEL_SIZE = (3,3)
 DATA_CRS = ccrs.PlateCarree()
 CMAP = [cmr.get_sub_cmap('binary_r', 0, 1), cmr.get_sub_cmap('PuBu', 0, 1), 
         cmr.get_sub_cmap('PuBu', 0, 1), cmr.get_sub_cmap('coolwarm', 0, 1)]
@@ -63,19 +70,26 @@ def plot_vars(dataframe, variables, date):
       t.append(list(dataframe.loc[l,:,date][variables[i]].values))
     ax = plt.subplot(math.ceil(num_var/2), math.ceil(num_var/2), i+1, projection=DATA_CRS)
     plot_var(ax, t, lon, lat, variables[i], CMAP[i], " ", cbar_location="left")  
-  plt.show()
-# plt.savefig("tcc.png")
+  # plt.show()
+  plt.savefig("tcc.png")
+
 
 # %% read data
 filepath_data = 'ERA5_tester_data.nc'
 data = xr.open_dataset(filepath_data)
 df = data.to_dataframe()
 
+
 # %% important variables
 check_date = '2010-01-01 00:00:00'
 var_list = ['tcc', 'p71.162', 'p72.162', 'p54.162'] # interest is always 0
 keys = ['longitude', 'latitude', 'time']
 num_data = df.shape[0]
+
+x_vars = []
+for i in range(len(var_list)):
+   x_vars.append(var_list[i])
+
 
 # %% Checking the data via plots
 plot_vars(df, var_list, check_date)
@@ -99,19 +113,37 @@ for var in var_list:
 #     INPUT_1D.append(cosvect.reshape(INPUT_2D.shape[0],1,1,1))
 #     INPUT_1D.append(sinvect.reshape(INPUT_2D.shape[0],1,1,1))
 
-# %% split to training and test (80 20?)
-rn.seed(123)
-# idx_train = rn.sample(range(num_data), int(0.8*num_data))
-# df_train = [df[k][idx_train,:,:,:] for k in range(num_var)]
-# df_test = [np.delete(df[k],idx_train,axis=0) for k in range(num_var)]
+# %% split to training, test and validation(80 20?)
+df_train, df_test, df_train_label, df_test_label = train_test_split(df[x_vars],
+                                                                      df[var_list[0]],
+                                                                      test_size=0.2,
+                                                                      random_state=13)
+df_train, df_valid, df_train_label, df_valid_label = train_test_split(df_train, 
+                                                                      df_train_label, 
+                                                                      test_size=0.2, 
+                                                                      random_state=13)
 
-# #split dataset into dependent and independent variables
-# df_train_dep = df_train[dependent]
-# df_train_ind = df_train[independent]
 
-# df_test_dep = df_test[dependent]
-# df_test_ind = df_test[independent]
+# %% train model -callbacks? optimizers or losses or activation
+model = Sequential()
+model.add(Conv2D(filters=NUM_FILTERS, kernel_size=KERNEL_SIZE, activation=ACTIVATION,
+                  input_shape=(9,9,1)))
 
-# %% train model
+for layer in NUM_LAYERS-1:
+   model.add(Conv2D(filters=NUM_FILTERS, kernel_size=KERNEL_SIZE, activation=ACTIVATION))
+   model.add(LeakyReLU(alpha=0.1))
+   model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(1, activation=ACTIVATION))
+
+model.compile(loss='mean_squares_error', optimizer='adam', metrics=['mean_absolute_error'])
+model.fit(df_train, df_train_label, epochs=EPOCH, batch_size=BS, 
+          validation_data=(df_valid, df_valid_label))
+
 
 # %% test model
+eval = model.evaluate(df_test, df_test_label)
+print('loss: ', eval[0])
+print('mae: ', eval[1])
